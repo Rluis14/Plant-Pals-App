7f90dd22dba8ec054443041833b3b25fbc71571c
Rluis14 <luis.gama@bellevuecollege.edu>
2025-06-09 16:10:34 -0700

    Fix routing warnings and JSON parse error

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,51 @@
+import { Tabs } from 'expo-router';
+import React from 'react';
+import { Platform } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+import { HapticTab } from '@/components/HapticTab';
+import TabBarBackground from '@/components/ui/TabBarBackground';
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: '#2F684E',
+        tabBarInactiveTintColor: '#A67B5B',
+        headerShown: false,
+        tabBarButton: HapticTab,
+        tabBarBackground: TabBarBackground,
+        tabBarStyle: Platform.select({
+          ios: {
+            position: 'absolute',
+          },
+          default: {
+            backgroundColor: '#fff',
+            borderTopColor: '#E6F2EA',
+          },
+        }),
+      }}>
+      <Tabs.Screen
+        name="mylist"
+        options={{
+          title: 'My List',
+          tabBarIcon: ({ color, size }) => <Ionicons name="heart-outline" color={color} size={size} />,
+        }}
+      />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Trending',
+          tabBarIcon: ({ color, size }) => <Ionicons name="trending-up-outline" color={color} size={size} />,
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: 'Search',
+          tabBarIcon: ({ color, size }) => <Ionicons name="search-outline" color={color} size={size} />,
+        }}
+      />
+    </Tabs>
+  );
+}
