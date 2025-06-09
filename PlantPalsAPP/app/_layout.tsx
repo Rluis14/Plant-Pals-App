7175454bd900044460539d412ed99bf7cbe6cbf9
Rluis14 <luis.gama@bellevuecollege.edu>
2025-06-09 16:05:17 -0700

    Restructure PlantPals Navigation Flow

--- PlantPalsAPP/app/_layout.tsx
+++ PlantPalsAPP/app/_layout.tsx
@@ -29,13 +29,12 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="_auth" options={{ headerShown: false }} />
-        <Stack.Screen name="search" options={{ headerShown: false }} />
-        <Stack.Screen name="trending" options={{ headerShown: false }} />
-        <Stack.Screen name="mylist" options={{ headerShown: false }} />
-        <Stack.Screen name="plant" options={{ headerShown: false }} />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="_auth" />
+        <Stack.Screen name="(tabs)" />
+        <Stack.Screen name="search" />
+        <Stack.Screen name="plant" />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
